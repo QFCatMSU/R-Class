c68acd7d4a257a71edd68e50f79c1df5f52695b9
belinskyc <belinsky@msu.edu>
2020-02-10 19:19:18 +0700

    Renamed Scripts folder modified 2.9 answer Added lesson 1-1 script and data

--- app_answers/2-9 Answers.R
+++ app_answers/2-9 Answers.R
@@ -13,34 +13,28 @@
   ###Part 1
   changeTempData = highTempData - lowTempData; 
   
-  hist(changeTempData, labels=TRUE, breaks=8, right=FALSE,     
+  hist(changeTempData, labels=TRUE, breaks=8, right=FALSE,  
       xlab="Change in Temp", col=c("purple", "lightblue"),
-      main="Change in Temperature over 14 days"); 
+      main="Change in Temperature over 14 days",
+      ylim= c(0,4.5));  # ylim is so the values on top of the bar do not get cut off 
   
   meanRange = mean(changeTempData);
   
   abline(v=meanRange, col="red", lty=4); 
   
   ###Part 2
- 
   allTempData = c(highTempData, lowTempData);
   
   barplot(allTempData,
       main = "High and Low Temperature Over 14 Days",
       xlab = "Dates", ylab = "Temperature (F)",
       angle=60, density=30,
-      col=c("purple", "black", "orange"));
+      col=c("purple", rgb(0,0,1), "orange"));
 
   meanTemp = mean(allTempData); 
   abline(h=meanTemp, col="dark green", lty=2);
-    
-  
   
   #Part 3
   boxplot((humidityData/100), precipData,
          names=c("Humidity", "Precipitation"));
-        
- 
-  
-  
 }
